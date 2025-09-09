e8bc4cbe0c56320d5ccfa0de509c6a388a8bc4de
crafanan29 <cr1084295@swccd.edu>
2025-09-09 20:10:06 +0000

    Created Maximum.java

---
+++ .devcontainer/Maximum.java
@@ -0,0 +1,51 @@
+/*
+ * This program determines the maxmimum number between 3 integers using if else statments
+ * @author Cameron Rafanan or @crafanan29 on GitHub
+ */
+import java.util.Scanner;
+
+public class Maximum 
+{
+    public static void main(String[] args)
+    {
+        int number1, number2, number3, max;             // Declaring integers
+        Scanner input = new Scanner(System.in);         // Creating a new Scanner
+
+        System.out.print("Enter the 1st integer: ");
+        number1 = input.nextInt();                      // ".nextInt()" because number1 is an int
+
+        System.out.print("Enter the 2nd integer: ");
+        number2 = input.nextInt();
+
+        System.out.print("Enter the 3rd integer: ");
+        number3 = input.nextInt();
+
+        //  Determine the maximum
+        if(number1 >= number2) 
+        {
+            max = number1;
+        }
+        else                                            // This line will run if number2 is greater than number1
+        {
+            max =number2;
+            if (max >= number3)                      // I did not know that I can put if statements inside else statements
+            {
+                max = number3;
+            }
+            else 
+            {
+                if (number1 >= number3)
+                {
+                    max = number1;
+                }
+                else
+                {
+                    max = number3;
+                }
+                
+             }
+        }
+        System.out.println("The maxmimum is "+ max);    //Prints the maxmimum value
+        input.close();                                  // Closes scanner
+    }
+}
